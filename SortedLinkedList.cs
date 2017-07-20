1a97fd5016b8d1c6dc56a2cd6c7dc08d4b2c06a6
Ned Murphy <Ned@TheLevelUp.com>
2017-07-20 17:50:58 -0400

    Implementing interfaces on SortedLinkedList

--- SortedLinkedList.cs
+++ SortedLinkedList.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Elevator
 {
-    public class SortedLinkedList<T> where T : IComparable
+    public class SortedLinkedList<T> : IEnumerable<T>, ICollection<T>
+        where T : IComparable
     {
         private LinkedList<T> _list;
         private Comparison<T> _comparisonLogic;
@@ -15,12 +16,19 @@ namespace Elevator
             _comparisonLogic = comparisonLogic;
         }
 
+        public int Count => _list == null ? 0 : _list.Count;
+
         public LinkedListNode<T> First => _list != null && _list.Count > 0 ? _list.First : null;
 
-        public int Count => _list == null ? 0 : _list.Count;
+        public bool IsReadOnly => ((ICollection<T>)_list).IsReadOnly;
 
         public virtual void Add(T element)
         {
+            if(null == element)
+            {
+                throw new ArgumentNullException("Cannot add null element to list!");
+            }
+
             if (_list.First == null)
             {
                 _list.AddFirst(element);
@@ -47,6 +55,26 @@ namespace Elevator
             }
         }
 
+        public void Clear()
+        {
+            ((ICollection<T>)_list).Clear();
+        }
+
+        public bool Contains(T element)
+        {
+            return _list.Contains(element);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            ((ICollection<T>)_list).CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_list).GetEnumerator();
+        }
+
         public void Remove(T element)
         {
             _list.Remove(element);
@@ -63,6 +91,16 @@ namespace Elevator
         public override string ToString()
         {            
             return $"[{string.Join(", ", _list)}]";
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)_list).GetEnumerator();
+        }
+
+        bool ICollection<T>.Remove(T item)
+        {
+            return ((ICollection<T>)_list).Remove(item);
         }
     }
 }
